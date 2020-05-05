b0deca172ee83d0a48dbca523be108eea4f04ebf
TRUONG Ngoc Tuan <tntuan0712494@gmail.com>
2020-05-05 10:53:26 +0700

    add mountebank sample;

---
+++ mb/index.js
@@ -0,0 +1,17 @@
+const mb = require("mountebank");
+const settings = require("./settings");
+const helloService = require("./hello-service");
+const customerService = require('./customer-service');
+
+const mbServerInstance = mb.create({
+  port: settings.port,
+  pidfile: "../mb.pid",
+  logfile: "../mb.log",
+  protofile: "../protofile.json",
+  ipWhitelist: ["*"],
+});
+
+mbServerInstance.then(function() {
+  helloService.addService();
+  customerService.addService();
+});
