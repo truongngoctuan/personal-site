e5a2e701366a3c3ca3fdf78c2bd2ec6d67c5373c
TRUONG Ngoc Tuan <tntuan0712494@gmail.com>
2020-05-06 14:28:07 +0700

    move project around, add backend;

---
+++ packages/crawler-puppeterr/GitHubActivity.cs
@@ -0,0 +1,50 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using PuppeteerSharp;
+
+namespace crawler_puppeterr
+{
+  public static class GitHubActivity
+  {
+    [FunctionName("GitHubActivity")]
+    public static async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+        ILogger log)
+    {
+      log.LogInformation("C# HTTP trigger function processed a request.");
+
+      await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultRevision);
+
+      string owner = req.Query["owner"];
+
+      var contributorsPage = $"https://github.com/{owner}/";
+
+      using (var browser = await Puppeteer.LaunchAsync(new LaunchOptions
+      {
+        Headless = true,
+        Args = new[] { "--no-sandbox" },
+        DefaultViewport = new ViewPortOptions
+        {
+          Width = 2000,
+          Height = 800
+        }
+      }))
+      using (var page = await browser.NewPageAsync())
+      {
+        await page.GoToAsync(contributorsPage);
+        await page.WaitForSelectorAsync(".graph-before-activity-overview");
+        var element = await page.QuerySelectorAsync(".graph-before-activity-overview");
+        var image = await element.ScreenshotDataAsync();
+
+        return new FileContentResult(image, "image/png");
+      }
+    }
+  }
+}
---
+++ packages/crawler-puppeterr/GoogleAudCurrency.cs
@@ -0,0 +1,55 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using PuppeteerSharp;
+
+namespace crawler_puppeterr
+{
+  public static class GoogleAudCurrency
+  {
+    const string query = "aud+to+vnd";
+
+    [FunctionName("GoogleAudCurrency")]
+    public static async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+        ILogger log)
+    {
+      log.LogInformation("C# HTTP trigger function processed a request.");
+
+      await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultRevision);
+
+      var queryPage = $"https://www.google.com/search?q={GoogleAudCurrency.query}";
+      Console.WriteLine(queryPage);
+      using (var browser = await Puppeteer.LaunchAsync(new LaunchOptions
+      {
+        Headless = true,
+        Args = new[] { "--no-sandbox" },
+        DefaultViewport = new ViewPortOptions
+        {
+          Width = 2000,
+          Height = 800
+        }
+      }))
+      using (var page = await browser.NewPageAsync())
+      {
+        await page.GoToAsync(queryPage);
+        // var image = await page.ScreenshotDataAsync();
+        await page.WaitForSelectorAsync("#knowledge-currency__updatable-data-column");
+        // var element = await page.QuerySelectorAsync("#knowledge-currency__updatable-data-column");
+        var element = await page.QuerySelectorAsync("[data-exchange-rate]");
+        var property = element.GetPropertyAsync("data-exchange-rate");
+        var attr = await element.EvaluateFunctionAsync("(obj) => obj.getAttribute('data-exchange-rate')");
+        
+        var exchangeRate = Decimal.Parse(attr.ToString());
+
+        return new JsonResult(exchangeRate);
+      };
+    }
+  }
+}
