0b815a19246064f0dbdaa163dd7f321ed09730f6
TRUONG Ngoc Tuan <tntuan0712494@gmail.com>
2020-06-18 03:56:19 +0700

    ++

---
+++ packages/aws-crawler-novels/domain/currency-crawler.js
@@ -0,0 +1,40 @@
+const chromium = require("chrome-aws-lambda");
+
+exports.crawCurrency = async function crawCurrency() {
+  let result = null;
+  let browser = null;
+
+  try {
+    browser = await chromium.puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      // headless: chromium.headless,
+      headless: true,
+      ignoreHTTPSErrors: true,
+    });
+
+    let page = await browser.newPage();
+
+    const query = "aud+to+vnd";
+    const queryPage = `https://www.google.com/search?q=${query}`;
+
+    await page.goto(queryPage);
+    var attr = await page.$eval("[data-exchange-rate]", (obj) =>
+      obj.getAttribute("data-exchange-rate")
+    );
+
+    result = parseFloat(attr);
+  } catch (error) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: error,
+      }),
+    };
+  } finally {
+    if (browser !== null) {
+      await browser.close();
+    }
+  }
+};
---
+++ packages/aws-crawler-novels/handler.js
@@ -0,0 +1,55 @@
+const chromium = require("chrome-aws-lambda");
+const currencyCrawler = require("./domain/currency-crawler");
+const sender = require("./domain/send-result");
+
+// https://bitsofco.de/how-to-use-puppeteer-in-a-netlify-aws-lambda-function/
+exports.endpoint = async (event, context) => {
+  let result = null;
+  let browser = null;
+
+  try {
+    browser = await chromium.puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      // headless: chromium.headless,
+      headless: true,
+      ignoreHTTPSErrors: true,
+    });
+
+    let page = await browser.newPage();
+
+    const query = "aud+to+vnd";
+    const queryPage = `https://www.google.com/search?q=${query}`;
+
+    await page.goto(queryPage);
+    var attr = await page.$eval("[data-exchange-rate]", (obj) =>
+      obj.getAttribute("data-exchange-rate")
+    );
+
+    result = parseFloat(attr);
+  } catch (error) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: error,
+      }),
+    };
+  } finally {
+    if (browser !== null) {
+      await browser.close();
+    }
+  }
+
+  await sender.sendResultToUrl("CrawlerCurrencies", {
+    currencyCode: "AUD",
+    value: result,
+  });
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: result,
+    }),
+  };
+};
